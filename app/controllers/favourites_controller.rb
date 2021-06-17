6dc148cfa01b5df5be2c22a4e283547ee3b92526
Alexia Danan <dananalexia@gmail.com>
2021-06-17 18:09:35 +0200

    favourites working

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -2,19 +2,42 @@ class FavouritesController < ApplicationController
 
   def index
     #TO DO get all nomads (type))
-    @nomads = User.all
+    @user = current_user
+    @nomads = @user.favourite_users
+    @cafes = @user.favourite_cafes
   end
 
 
   def show
 
+
   end
 
 
   def new
+
+  end
+
+  def create
+    @new_fav = Favourite.new(fav_params)
+    @new_fav.user = current_user
+    @new_fav.save!
+
+    redirect_to nomad_community_path
+
+
+
+
   end
 
   def destroy
+  end
+
+  private
+
+  def fav_params
+    params.require(:favourite).permit(:favouritable_type, :favouritable_id)
+
   end
 
 end
