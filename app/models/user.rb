ae7a44c9d08565b06a262e83b48316e3e4d2ce7a
Alexia Danan <dananalexia@gmail.com>
2021-06-23 16:54:27 +0200

    notifications- just needs more styling now

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,25 +9,20 @@ class User < ApplicationRecord
   has_many :credits, dependent: :destroy
   has_one :cafe, dependent: :destroy
   has_one_attached :photo
-  validates :nomacafe_type, presence: true, inclusion: {in: ["cafe", "nomad"]}
+  validates :nomacafe_type, presence: true, inclusion: { in: ["cafe", "nomad"] }
 
   has_many :favourites
   has_many :favourite_cafes, source: :favouritable, source_type: "Cafe", through: :favourites, foreign_key: :favouritable_id
   has_many :favourite_users, source: :favouritable, source_type: "User", through: :favourites, foreign_key: :favouritable_id
 
-  has_many :favourites_as_favouritable, class_name:"Favourite", as: :favouritable
+  has_many :favourites_as_favouritable, class_name: "Favourite", as: :favouritable
   has_many :orders
-
 
   def is_cafe_owner?
     cafe.present?
-
   end
 
   def follows?(favouritable)
     favourites.where(favouritable: favouritable, user: self).any?
   end
-
-
-
 end
