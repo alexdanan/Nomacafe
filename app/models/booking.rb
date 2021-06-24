b4a665e5d9dcecb79d0fd07e3031e36c69890c31
Andrea Rocca <andrea.rocca3@gmail.com>
2021-06-24 11:10:11 +0200

    Revert

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,38 +1,20 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :table
-  has_one :review, dependent: :destroy
+  has_one :review, dependent: :nullify
   has_one :cafe, through: :table
   enum status: [:pending, :accepted, :declined]
+  
   before_create :set_booking_times
   before_create :set_owed_money
-  after_update :notify_cafe_owner
-  SLOTS = { morning: { start: 11, end: 15 }, afternoon: { start: 15, end: 19 } }
-
-  acts_as_notifiable :users,
-  targets: ->(booking, key) {
-    if key == "booking.owner"
-      [booking.cafe.user]
-    else
-      users = booking.table.bookings_by_slot(booking.start_time, booking.parse_slot(booking.slots.first)).map {|booking| booking.user}
-      users - [booking.user]
-    end
-    },
-    notifiable_path: :booking_notifiable_path
-
-  def booking_notifiable_path
-    dashboard_path
-  end
+  
+  SLOTS = {morning: {start: 11, end: 15}, afternoon: {start: 15, end: 19}}
 
   def booking_headline
-    "Your seat has been #{status} at #{table.cafe.name}
-    located at #{table.cafe.location}. You have a #{table.name}
-    with a #{table.seat}
+    "Your seat has been #{status} at #{table.cafe.name} 
+    located at #{table.cafe.location}. You have a #{table.name} 
+    with a #{table.seat} 
     costing a total of €#{table.min_credits}"
-  end
-
-  def parse_slot(slot)
-    slot.split("_").first
   end
 
   def set_booking_times
@@ -45,13 +27,6 @@ class Booking < ApplicationRecord
       table.cafe.increment!(:owed_money, table.min_credits)
     else
       table.cafe.increment!(:owed_money, table.min_credits * 2)
-    end
-  end
-
-  def notify_cafe_owner
-    if accepted?
-      notify :users, key: "booking.owner"
-      notify :users, key: "booking.table.nomads"
     end
   end
 end
